eacb27a99615bacd47d9c9ac39358541c5cc5352
Dmitriy Okoneshnikov <magicwinnie@yandex.ru>
2025-03-21 03:00:38 +0300

    Lab 2.05 & 2.06

--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -49,22 +49,18 @@ void cg::renderer::ray_tracing_renderer::update() {}
 
 void cg::renderer::ray_tracing_renderer::render()
 {
-	shadow_raytracer->miss_shader = [](const ray& ray) {
-		payload payload{};
-		payload.t = -1.f;
-		return payload;
-	};
-	shadow_raytracer->any_hit_shader = [](const ray& ray, payload& payload, const triangle<cg::vertex>& triangle) {
-		return payload;
-	};
-	shadow_raytracer->build_acceleration_structure();
-
 	raytracer->clear_render_target({0, 0, 0});
 	raytracer->miss_shader = [](const ray& ray) {
 		payload payload{};
-		payload.color = {0.f, 0.f, (ray.direction.y + 1.f) * 0.5f};
+		payload.color = {0.f, 0.f, 0.f};
 		return payload;
 	};
+
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<float> dis(-1.f, 1.f);
+
+
 	raytracer->closest_hit_shader = [&](const ray& ray, payload& payload, const triangle<cg::vertex>& triangle, size_t depth) {
 		float3 position = ray.position + ray.direction * payload.t;
 		float3 normal = normalize(
@@ -73,13 +69,15 @@ void cg::renderer::ray_tracing_renderer::render()
 				payload.bary.z * triangle.nc);
 		float3 result_color = triangle.emissive;
 
-		for (auto& light: lights) {
-			cg::renderer::ray to_light(position, light.position - position);
-			auto shadow = shadow_raytracer->trace_ray(to_light, 1, length(light.position - position));
-			if (shadow.t < 0.f) {
-				result_color += triangle.diffuse * light.color * std::max(0.f, dot(normal, to_light.direction));
-			}
+		float3 random_direction = float3{dis(gen), dis(gen), dis(gen)};
+		if (dot(normal, random_direction) < 0.f) {
+			random_direction = -random_direction;
 		}
+
+		cg::renderer::ray to_next_object(position, random_direction);
+		auto next_payload = raytracer->trace_ray(to_next_object, depth);
+
+		result_color += triangle.diffuse * next_payload.color.to_float3() * std::max(0.f, dot(normal, to_next_object.direction));
 
 		payload.color = cg::color::from_float3(result_color);
 		return payload;
@@ -100,7 +98,4 @@ void cg::renderer::ray_tracing_renderer::render()
 	std::cout << "Raytracing took " << duration.count() << " ms\n";
 
 	cg::utils::save_resource(*render_target, settings->result_path);
-
-	// TODO Lab: 2.05 Adjust `ray_tracing_renderer` class to build the acceleration structure
-	// TODO Lab: 2.06 (Bonus) Adjust `closest_hit_shader` for Monte-Carlo light tracing
 }
